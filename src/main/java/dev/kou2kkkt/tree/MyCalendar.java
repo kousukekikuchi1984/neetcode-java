06472f7bae246b7073decf99a504b62e11a01903
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-05-06 05:56:42 +0900

    added MyCalendar

--- src/main/java/dev/kou2kkkt/tree/MyCalendar.java
+++ src/main/java/dev/kou2kkkt/tree/MyCalendar.java
@@ -1,12 +1,48 @@
 package dev.kou2kkkt.tree;
 
-
 class MyCalendar {
-    public MyCalendar() {
+    // ref: https://leetcode.com/problems/my-calendar-i/
+    int L;
+    int R;
+    MyCalendar left;
+    MyCalendar right;
 
+    public MyCalendar() {
+        this.L = -1;
+        this.R = -1;
+        this.left = null;
+        this.right = null;
+    }
+
+    private boolean within(int start, int end) {
+        if (this.L == -1 || this.R == -1) {
+            return false;
+        }
+        // revert without
+        return !(end <= this.L || this.R <= start);
     }
 
     public boolean book(int start, int end) {
-
+        if (this.within(start, end)) {
+            return false;
+        }
+        if (this.L == -1 || this.R == -1) {
+            this.L = start;
+            this.R = end;
+            return true;
+        }
+        if (start >= this.R) {
+            if (this.left == null) {
+                this.left = new MyCalendar();
+            }
+            return this.left.book(start, end);
+        }
+        if (end <= this.L) {
+            if (this.right == null) {
+                this.right = new MyCalendar();
+            }
+            return this.right.book(start, end);
+        }
+        return false;
     }
 }
