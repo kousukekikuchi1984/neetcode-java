dd2e00b0b17ea8a52589bba9355048886b690ccd
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-05-07 07:01:18 +0900

    added findMaximizedCapital

--- src/main/java/dev/kou2kkkt/heap/Solution.java
+++ src/main/java/dev/kou2kkkt/heap/Solution.java
@@ -1,4 +1,7 @@
 package dev.kou2kkkt.heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
 class Solution {
     public double[] medianSlidingWindow(int[] nums, int k) {
         double[] ans = new double[nums.length - k + 1];
@@ -12,6 +15,27 @@ class Solution {
         return ans;
     }
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int total_capital = w;
+        PriorityQueue<int[]> maxProfits = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        PriorityQueue<int[]> minCapital = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        for (int i = 0; i < profits.length; i++) {
+            minCapital.add(new int[]{capital[i], profits[i]});
+        }
 
+        for (int i = 0; i < profits.length; i++) {
+            if (k == 0) {
+                break;
+            }
+            while (!minCapital.isEmpty() && minCapital.peek()[0] <= total_capital) {
+                maxProfits.add(minCapital.poll());
+            }
+            if (maxProfits.isEmpty()) {
+                break;
+            }
+            total_capital += maxProfits.poll()[1];
+            k--;
+        }
+
+        return total_capital;
     }
 }
