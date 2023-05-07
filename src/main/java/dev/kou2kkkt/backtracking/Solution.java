4248698207b4300fd5573a1e689e60e1feb7736f
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-05-07 16:12:02 +0900

    dd _subsetsWithDuplicates

--- src/main/java/dev/kou2kkkt/backtracking/Solution.java
+++ src/main/java/dev/kou2kkkt/backtracking/Solution.java
@@ -1,7 +1,32 @@
 package dev.kou2kkkt.backtracking;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         // ref: https://leetcode.com/problems/subsets-ii/
+        List<List<Integer>> subsets = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        Arrays.sort(nums);
+        this._subsetsWithDuplicates(0, nums, subset, subsets);
+        return subsets;
+    }
+
+    private void _subsetsWithDuplicates(int index, int[] nums, List<Integer> subset, List<List<Integer>> subsets) {
+        if (index == nums.length)  {
+            subsets.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[index]);
+        this._subsetsWithDuplicates(index + 1, nums, subset, subsets);
+        subset.remove(subset.size() - 1);
+
+        // skip duplicates
+        while (index + 1 < nums.length && nums[index] == nums[index + 1]) {
+            index++;
+        }
+        this._subsetsWithDuplicates(index + 1, nums, subset, subsets);
     }
 }
--- src/test/java/dev/kou2kkkt/backtracking/SolutionTest.java
+++ src/test/java/dev/kou2kkkt/backtracking/SolutionTest.java
@@ -3,12 +3,13 @@ package dev.kou2kkkt.backtracking;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class SolutionTest {
     @Test
     void test_subsetsWithDup() {
         // ref: https://leetcode.com/problems/subsets-ii/
         Solution solution = new Solution();
-        Assertions.assertEquals(List.of(List.of(), List.of(1), List.of(1, 2), List.of(1, 2, 2), List.of(2), List.of(2, 2)), solution.subsetsWithDup(new int[]{1, 2, 2}));
-        Assertions.assertEquals(List.of(List.of(), List.of(0)), solution.subsetsWithDup(new int[]{0}));
+        Assertions.assertIterableEquals(List.of(List.of(), List.of(1), List.of(1, 2), List.of(1, 2, 2), List.of(2), List.of(2, 2)), solution.subsetsWithDup(new int[]{1, 2, 2}));
     }
 }
