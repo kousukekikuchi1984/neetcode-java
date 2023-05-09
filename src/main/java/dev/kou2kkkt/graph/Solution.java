6844c23e6f007a80a89e787423cce181bbff654d
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-05-10 06:11:58 +0900

    added skelton for networkDelayTime

---
+++ src/main/java/dev/kou2kkkt/graph/Solution.java
@@ -0,0 +1,8 @@
+package dev.kou2kkkt.graph;
+
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        // ref: https://leetcode.com/problems/network-delay-time/
+        return 0;
+    }
+}
---
+++ src/test/java/dev/kou2kkkt/graph/SolutionTest.java
@@ -0,0 +1,10 @@
+package dev.kou2kkkt.graph;
+
+class SolutionTest {
+    @Test
+    void test_networkDelayTime() {
+        // ref: https://leetcode.com/problems/network-delay-time/
+        Solution solution = new Solution();
+        Assertions.assertEquals(2, solution.networkDelayTime(new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2));
+    }
+}
